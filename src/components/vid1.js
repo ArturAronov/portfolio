4479aac02f760dec982837b789da04db7b1b6282
arturaronov <unionco@icloud.com>
2021-07-19 01:55:23 +0800

    working on function that makes video element turn invisible on mouse enter

--- src/components/vid1.js
+++ src/components/vid1.js
@@ -1,9 +1,22 @@
 import Video from '../vids/1.mp4'
 
 export const Vid1=function(){
+  const test=function(){
+    let element=document.getElementById('v1').style;
+    //let viz=element.visibility='hidden'
+    //window.getComputedStyle(document.getElementById('v1')).visibility
+    const viz=window.getComputedStyle(document.getElementById('v1'));
+    
+    console.log(viz.getPropertyValue('visibility'))
+    element.visibility='hidden';
+    console.log(viz.getPropertyValue('visibility'))
+    element.visibility='visible';
+    console.log(viz.getPropertyValue('visibility'))
+  };
+
   return(
     <div id='v1'>
-      <video autoPlay muted loop src={Video} type='video/mp4'/>
+      <video autoPlay muted loop src={Video} type='video/mp4' onMouseOver={()=>test()}/>
     </div>
   );
 };
